ec59c6ed6f68b18e27fe5133ffe7844cd436ec74
amanm4 <amanm4@localhost>
2023-05-28 02:10:07 -0600

    adding script for deps

---
+++ setup_deps.sh
@@ -0,0 +1,17 @@
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+
+sudo apt install -y g++-11
+
+g++-11 --version
+
+
+sudo apt install apt-transport-https curl gnupg -y
+curl -fsSL https://bazel.build/bazel-release.pub.gpg | gpg --dearmor >bazel-archive-keyring.gpg
+sudo mv bazel-archive-keyring.gpg /usr/share/keyrings
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/bazel-archive-keyring.gpg] https://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+
+sudo apt update && sudo apt install bazel
+
+sudo apt update && sudo apt full-upgrade
+
+sudo apt update && sudo apt install bazel-5.4.0
